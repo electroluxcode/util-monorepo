34da9c54cbe5b2c69eba7055d726be8f95b06c54
Electrolux <3451613934@qq.com>
2023-12-11 15:44:50 +0800

    fix(fetch):解决fetch 数据的 before request是 formdata拿不到值的问题

--- behind/other/app.js
+++ behind/other/app.js
@@ -27,11 +27,22 @@ app.use('/users', usersRouter);
 
 // 跨域
 app.all('*',function(req,res,next){
-  res.header('Access-Control-Allow-Origin', '*');//的允许所有域名的端口请求（跨域解决）
-  res.header("Access-Control-Allow-Headers", "content-type,Token,X-Requested-With,Content-Type");
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5501');//的允许所有域名的端口请求（跨域解决）
+  res.header("Access-Control-Allow-Headers", "content-type,Token,X-Requested-With,Content-Type,My-Header");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   res.setHeader("Access-Control-Max-Age", "3600");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  // 不带凭据的请求响应通配符
+  res.setHeader("Access-Control-Expose-Headers", '*');
+  // res.setHeader("server-test", '454545');
   console.log("经过")
+  // res.setHeader("Set-Cookie", 'iddddddd=a3f;SameSite=None');
+  
+  // HttpOnly; 前端无法操作
+  // Secure https 才能访问
+  // res.setHeader("Set-Cookie", "idd=a32222;Path=/;Secure;SameSite=None")
+
+  // res.setHeader("Set-Cookie", "id=7sssd822;Path=/;Secure;SameSite=None;Domain=.baidu.com;")
   next();
 })
 app.use(bodyParser.text());
@@ -98,6 +109,11 @@ app.get('/api/get', function(req, res) {
 
 //post 接收参数测试 application/json 的有效 {id:56}
 app.all('/api/post', function(req, res) {
+  // res.setHeader('Set-Cookie', 'servercookie=78787788')
+  // res.location('http://baidu.com')
+  res.redirect(200, 'http://baidu.com');
+  res.setHeader("Set-Cookie", "id=7sssd822;Path=/;Secure;SameSite=None;Domain=.baidu.com;")
+  // return
   // json 的才有效
   res.send({
     code:300,
@@ -143,8 +159,8 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage });
 app.post("/api/file", upload.single('file'), (req, res) => {
-  console.log('req', req.file);
-  res.json({ url: `http://localhost:8088/public/` })
+  console.log('req', req.files);
+  res.json({ url: `http://localhost:8088/public/${req.file}` })
 })
 
 // get 发送短信 

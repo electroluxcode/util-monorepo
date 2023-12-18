35c9ef9a61795820ce7a673c5433b1a773ccc537
Electrolux <3451613934@qq.com>
2023-12-18 11:18:20 +0800

    fix:完全解决ScreenScale的偏移问题

--- commonjs_build/ui/ScreenScale/ScreenScale.js
+++ commonjs_build/ui/ScreenScale/ScreenScale.js
@@ -84,14 +84,14 @@ class ScreenScale {
                 if (this.IsMapElement) {
                     setTimeout(() => {
                         this.FixMap(this.CurrelFixMap, this.CurrelFixMapLevel);
-                    }, 1000);
+                    }, 0);
                 }
                 ;
             }
             // this.updateElementSize()
         };
         // this.resizeListener
-        let enhanceFn = debounce(this.resizeListener, 500);
+        let enhanceFn = debounce(this.resizeListener, 30);
         resize && window.addEventListener('resize', () => { enhanceFn(""); });
         // resize && window.addEventListener('resize',this.resizeListener);
         // enhanceFn("")
@@ -121,6 +121,7 @@ class ScreenScale {
         this.CurrelFixMap = el;
         this.CurrelFixMapLevel = level;
         for (let i in this.CurrelFixMap) {
+            console.log(this.CurrelFixMap);
             let item = document.querySelector(this.CurrelFixMap[i]);
             // let item = this.CurrelFixMap[i] as any
             if (!item) {
@@ -130,98 +131,27 @@ class ScreenScale {
             if (!this.IsMapElement) {
                 item.originalWidth = item.clientWidth;
                 item.originalHeight = item.clientHeight;
-                item.originBounding = item.getBoundingClientRect();
-                console.log("初次渲染fixmap:", {
-                    originalWidth: item.clientWidth,
-                    originalHeight: item.clientHeight,
-                    originBounding: item.getBoundingClientRect()
-                });
             }
-            // (item as any).originalWidth = item.clientWidth;
-            // (item as any).originalHeight = item.clientHeight;
-            // (item as any).originalHeight = item.clientHeight;
-            let w1 = this.CurrelScaleRadio.NowRadio.width / this.CurrelScaleRadio.initRadio.width;
-            let h1 = this.CurrelScaleRadio.NowRadio.height / this.CurrelScaleRadio.initRadio.height;
-            let radio1 = this.CurrelScaleRadio.NowRadio.width / this.CurrelScaleRadio.NowRadio.height;
-            let radio2 = this.CurrelScaleRadio.initRadio.width / this.CurrelScaleRadio.initRadio.height;
-            let radio3 = item.originalWidth / item.originalHeight;
-            // 得到变化尺寸 用于自定义
-            const rectification = this.currScale === 1 ? 1 : this.currScale * Number(level);
-            // 不等比例缩放
-            // 下面是等比例缩放的情况
-            // let radio = this.CurrelScaleRadio.initRadio / this.CurrelScaleRadio.NowRadio;
             // 变化的时候 会优先满足 最短的一条边长(高) * rectification * rectification
             item.style.transform = `scale(${1 / this.currScale}) `;
-            // console.log({
-            //       "现在的高度比例": this.testVar.heightRadio ,
-            //       "现在的宽度比例": this.testVar.widthRadio
-            //       // faceHeight: `${(item as any).originalHeight * this.currScale }px `,
-            //       // factWidth: `${(item as any).originalWidth* this.currScale   }px `
-            //     },{radio1,radio2,w1,h1,currScale:this.currScale,CurrelScaleRadio:this.CurrelScaleRadio});
-            // /w1 * h1
-            // if() * this.currScale
             item.style.transformOrigin = `0 0`;
-            // let countWidth =(item as any).originalWidth / this.options.dw
-            // (item as any).style.height = `${(item as any).originalHeight * this.currScale}px `;
-            // (item as any).style.width = `${(item as any).originalWidth * this.currScale }px `;
-            // this.dom.style.setProperty('--scale', 1/this.currScale);
-            // document.documentElement.style.setProperty('--scale', String(this.currScale));
-            //     let clientWidth =(item as any).originalWidth * this.currScale
-            //     let clientHeight = (item as any).originalHeight * this.currScale
-            // let currScale = clientWidth / clientHeight < radio1 ?  clientHeight / this.CurrelScaleRadio.NowRadio.height
-            // (item as any).style.height = `${(item as any).originalHeight }px `;
-            // (item as any).style.width = `${(item as any).originalWidth  }px `;
-            // +100*this.testVar.heightRad   /w1 * h1
-            // (item as any).style.height = `${(item as any).originalHeight*(this.testVar.heightRadio  ?? 1) }px `;
-            // (item as any).style.width = `${((item as any).originalWidth)*(this.testVar.widthRadio ?? 1)  }px `;
-            // this.CurrelScaleRadio.initRadio.width
-            let radioHeight = item.originalHeight / this.options.dh;
-            // let radioWidth = ((item as any).originalWidth)/this.options.dw!;
-            // let radioHeight = 1;
-            let radioWidth = (item.originalWidth) / this.options.dw;
-            //得到比例
-            console.log("zptest:", {
-                // "初始容器高度比":(item as any).originalHeight/this.options.dh,
-                // "初始容器宽度比":(item as any).originalWidth/this.options.dw,
-                "目前大容器高度": document.documentElement.clientHeight,
-                "目前大容器宽度": document.documentElement.clientWidth,
-                "目前大容器": this.dom,
-                "radioHeight": [item.originalHeight, this.options.dh, radioHeight],
-                "实际高": `${radioHeight * this.dom.offsetHeight * (this.currScale)}`,
-                "radio1": radio1,
-                "this.currScale": this.currScale,
-                radio3: radio3
-            });
-            // (item as any).style.height = `${(item as any).originalHeight/this.options.dh! *document.documentElement.clientHeight}px `;
-            if (radio2 > radio3) {
-            }
-            // let realHeight = 
-            let containerHeight = document.querySelector(this.options.el).getBoundingClientRect().height;
-            let containerTop = item.getBoundingClientRect().top;
-            let containerBottom = (item.getBoundingClientRect().bottom - item.getBoundingClientRect().height);
             document.querySelector(this.options.el).addEventListener("transitionend", () => {
-                item = document.querySelector(this.CurrelFixMap[i]);
-                let containerHeight = document.querySelector(this.options.el).getBoundingClientRect().height;
-                let containerTop = item.getBoundingClientRect().top;
-                let containerBottom = (item.getBoundingClientRect().bottom - item.getBoundingClientRect().height);
-                item.style.height = `${containerHeight - containerTop - containerBottom}px   `;
-                // (item as any).style.height = `${this.currScale *(item as any).originalHeight}px  `;
-                let containerWidth = document.querySelector(this.options.el).getBoundingClientRect().width;
-                let containerLeft = item.getBoundingClientRect().left;
-                let containerRight = (document.querySelector(this.options.el).getBoundingClientRect().width - item.getBoundingClientRect().right);
-                console.log("触发");
-                item.style.width = `${containerWidth - containerRight - containerLeft}px   `;
+                // item = document.querySelector(this.CurrelFixMap[i])!
+                // let containerHeight = document.querySelector(this.options.el!)!.getBoundingClientRect().height;
+                // let containerTop = item!.getBoundingClientRect().top;
+                // let containerBottom =( containerHeight-item!.getBoundingClientRect().height-item!.getBoundingClientRect().top) ;
+                // (item as any).style.height = `${containerHeight -containerTop-containerBottom }px   `;
+                // let containerWidth = document.querySelector(this.options.el!)!.getBoundingClientRect().width;
+                // let containerLeft = item!.getBoundingClientRect().left;
+                // // let containerRight =( item!.getBoundingClientRect().right-item!.getBoundingClientRect().width) ;
+                // let containerRight =( containerWidth-item!.getBoundingClientRect().left-item!.getBoundingClientRect().width) ;
+                // (item as any).style.width = `${containerWidth-containerRight-containerLeft }px   `;
+                // debugger
             });
-            item.style.height = `${containerHeight - containerTop - containerBottom}px   `;
-            // (item as any).style.height = `${this.currScale *(item as any).originalHeight}px  `;
-            item.style.width = `${this.currScale * item.originalWidth}px  `;
-            // debugger
-            // (item as any).style.height = `${radioHeight *this.dom.offsetHeight*(this.currScale)}px  `;
-            // (item as any).style.width = `${radioWidth*document.documentElement.clientWidth  }px`;
-            // (item as any).style.height = `${radioHeight *this.dom.offsetHeight*(this.currScale)}px  `;
-            // (item as any).style.width =  `${radioWidth *this.dom.offsetWidth*(this.currScale)}px  `;
-            // (item as any).style.height = `${(item as any).originalHeight/h1 * w1  }px `;
-            // (item as any).style.width = `${((item as any).originalWidth)/w1 * h1 }px `;
+            item.style.height = `${this.currScale * 100}%`;
+            item.style.width = `${this.currScale * 100}%`;
+            // (item as any).style.height = `${item.clientHeight}px`;
+            // (item as any).style.width = `${item.clientWidth}px`;
         }
         this.IsMapElement = true;
     }
@@ -229,31 +159,52 @@ class ScreenScale {
      * @des 调整大小 | 自适应最小的 边长
      */
     KeepFit(dw, dh, domStr, ignore) {
-        // dom.style.transform = ``;
         let dom = document.querySelector(domStr);
         const clientHeight = document.documentElement.clientHeight;
         const clientWidth = document.documentElement.clientWidth;
-        // document.body.getBoundingClientRect()
-        // const clientHeight = document.body.getBoundingClientRect().height;
-        // const clientWidth = document.body.getBoundingClientRect().width;
-        this.currScale = clientWidth / clientHeight < dw / dh ? clientWidth / dw : clientHeight / dh;
-        this.testVar = {
-            widthRadio: clientWidth / dw,
-            heightRadio: clientHeight / dh,
-        };
-        if (clientWidth / clientHeight < dw) {
-            // dom.style.height = `${clientHeight / this.currScale}px`;
-        }
-        else {
-            // dom.style.width = `${clientWidth / this.currScale}px`;
-        }
-        // dom.style.height =`${clientHeight /  ( clientWidth / dw) }px` ;
-        // dom.style.width =  `${clientWidth / (clientHeight / dw)}px`;
+        this.currScale = (clientWidth / clientHeight) < (dw / dh) ? clientWidth / dw : clientHeight / dh;
+        // if((clientWidth / clientHeight)< (dw / dh)){
+        //   let curr = clientWidth / dw
+        //   dom.style.height = `${clientHeight / this.currScale}px`;
+        //   dom.style.width = `${clientWidth / this.currScale}px`;  
+        // }else{
+        //   let curr = clientWidth / dw
+        // dom.style.height = `${clientHeight / this.currScale}px`;
+        // dom.style.width = `${clientWidth / this.currScale}px`;
+        // }
         dom.style.height = `${clientHeight / this.currScale}px`;
         dom.style.width = `${clientWidth / this.currScale}px`;
         dom.style.transform = `scale(${this.currScale})`;
         const ignoreStyleDOM = document.querySelector('#ignoreStyle');
         ignoreStyleDOM.innerHTML = '';
+        for (let item of ignore) {
+            let itemEl = item.el || item.dom;
+            typeof item == "string" && (itemEl = item);
+            if (!itemEl) {
+                console.error(`autofit: bad selector: ${itemEl}`);
+                continue;
+            }
+            let realScale = item.scale ? item.scale : 1 / this.currScale;
+            let realFontSize = realScale != this.currScale ? item.fontSize : "autofit";
+            let realWidth = realScale != this.currScale ? item.width : "autofit";
+            let realHeight = realScale != this.currScale ? item.height : "autofit";
+            let regex = new RegExp(`${itemEl}(\x20|{)`, "gm");
+            let isIgnored = regex.test(ignoreStyleDOM.innerHTML);
+            if (isIgnored) {
+                continue;
+            }
+            ignoreStyleDOM.innerHTML += `\n${itemEl} { 
+        transform: scale(${realScale})!important;
+        transform-origin: 0 0;
+        width: ${realWidth}!important;
+        height: ${realHeight}!important;
+      }`;
+            if (realFontSize) {
+                ignoreStyleDOM.innerHTML += `\n${itemEl} div ,${itemEl} span,${itemEl} a,${itemEl} * {
+          font-size: ${realFontSize}px;
+        }`;
+            }
+        }
     }
 }
 exports.ScreenScale = ScreenScale;

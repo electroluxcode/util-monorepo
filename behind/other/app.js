a38e2e87acb70abc822e74076f8da46609fd00d7
Electrolux <3451613934@qq.com>
2024-01-18 15:02:34 +0800

    refactor:暂存支付接口

--- behind/other/app.js
+++ behind/other/app.js
@@ -1,205 +1,207 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const http = require('http');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var SendMail = require('./config/email');
-const multer = require('multer')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const http = require("http");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var SendMail = require("./config/email");
+const multer = require("multer");
 const schedule = require("node-schedule");
 var app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // 跨域
-app.all('*',function(req,res,next){
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5501');//的允许所有域名的端口请求（跨域解决）
-  res.header("Access-Control-Allow-Headers", "content-type,Token,X-Requested-With,Content-Type,My-Header");
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Max-Age", "3600");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  // 不带凭据的请求响应通配符
-  res.setHeader("Access-Control-Expose-Headers", '*');
-  // res.setHeader("server-test", '454545');
-  console.log("经过")
-  // res.setHeader("Set-Cookie", 'iddddddd=a3f;SameSite=None');
-  
-  // HttpOnly; 前端无法操作
-  // Secure https 才能访问
-  // res.setHeader("Set-Cookie", "idd=a32222;Path=/;Secure;SameSite=None")
+app.all("*", function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5501"); //的允许所有域名的端口请求（跨域解决）
+	res.header(
+		"Access-Control-Allow-Headers",
+		"content-type,Token,X-Requested-With,Content-Type,My-Header"
+	);
+	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+	res.setHeader("Access-Control-Max-Age", "3600");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	// 不带凭据的请求响应通配符
+	res.setHeader("Access-Control-Expose-Headers", "*");
+	// res.setHeader("server-test", '454545');
+	console.log("经过");
+	// res.setHeader("Set-Cookie", 'iddddddd=a3f;SameSite=None');
 
-  // res.setHeader("Set-Cookie", "id=7sssd822;Path=/;Secure;SameSite=None;Domain=.baidu.com;")
-  next();
-})
+	// HttpOnly; 前端无法操作
+	// Secure https 才能访问
+	// res.setHeader("Set-Cookie", "idd=a32222;Path=/;Secure;SameSite=None")
+
+	// res.setHeader("Set-Cookie", "id=7sssd822;Path=/;Secure;SameSite=None;Domain=.baidu.com;")
+	next();
+});
 app.use(bodyParser.text());
 
+// 自动签到脚本
+const JueJinSignIn = require("./signin/juejin");
 
+const Sign = async () => {
+	let log = {
+		today: "今天要有一个好心情哦",
+		juejin: "无数据",
+	};
+	// 1.juejin
+	try {
+		log["juejin"] = await JueJinSignIn();
+	} catch {
+		log["juejin"] = await JueJinSignIn();
+	}
 
-// 自动签到脚本 
-const JueJinSignIn = require("./signin/juejin")
-
-const Sign = async() =>{
-  let log = {
-    today:"今天要有一个好心情哦",
-    juejin:"无数据",
-
-  }
-  // 1.juejin
-  try{
-    log["juejin"] = await JueJinSignIn()
-  }catch{
-    log["juejin"] = await JueJinSignIn()
-  }
-  
-  let text = ``
-  // 组装
-  for(let i in log){
-    text += `<li>${i}:${log[i]}</li>`
-  }
-  let ResText = `<ul>${text}</ul>`
-  let ResTitle = `我的日志`
-  SendMail(ResTitle,"electroluxcode@gmail.com",ResText)
-}
-// 秒、分、时、日、月、周几 
+	let text = ``;
+	// 组装
+	for (let i in log) {
+		text += `<li>${i}:${log[i]}</li>`;
+	}
+	let ResText = `<ul>${text}</ul>`;
+	let ResTitle = `我的日志`;
+	SendMail(ResTitle, "electroluxcode@gmail.com", ResText);
+};
+// 秒、分、时、日、月、周几
 // '*'表示通配符，匹配任意，当秒是'*'时，表示任意秒数都触发，其它类推
 schedule.scheduleJob("0 24 06 * * *", () => {
-  setTimeout(() => {
-    Sign(); //签到函数
-  }, Math.random() * 10 * 60 * 1000)
-  
-})
+	setTimeout(() => {
+		Sign(); //签到函数
+	}, Math.random() * 10 * 60 * 1000);
+});
 
-
-// Sign()
-
-
-
-
+Sign();
 
 //get 接受参数测试
-app.get('/api/get', function(req, res) {
-  if(req.query){
-    let data = JSON.parse(JSON.stringify(req.query))
-    console.log(data)
-  }
-  setTimeout(() => {
-    res.send({
-      code:200,
-      msg:Math.random()+`
+app.get("/api/get", function (req, res) {
+	if (req.query) {
+		let data = JSON.parse(JSON.stringify(req.query));
+		console.log(data);
+	}
+	setTimeout(() => {
+		res.send({
+			code: 200,
+			msg:
+				Math.random() +
+				`
       ddddddddddddddddd
       `,
-    });
-  }, 1000);
-  
+		});
+	}, 1000);
 });
 
 //post 接收参数测试 application/json 的有效 {id:56}
-app.all('/api/post', function(req, res) {
-  // res.setHeader('Set-Cookie', 'servercookie=78787788')
-  // res.location('http://baidu.com')
-  res.redirect(200, 'http://baidu.com');
-  res.setHeader("Set-Cookie", "id=7sssd822;Path=/;Secure;SameSite=None;Domain=.baidu.com;")
-  // return
-  // json 的才有效
-  res.send({
-    code:300,
-    msg:"post_success",
-    data:req.body
-  });
+app.all("/api/post", function (req, res) {
+	// res.setHeader('Set-Cookie', 'servercookie=78787788')
+	// res.location('http://baidu.com')
+	res.redirect(200, "http://baidu.com");
+	res.setHeader(
+		"Set-Cookie",
+		"id=7sssd822;Path=/;Secure;SameSite=None;Domain=.baidu.com;"
+	);
+	// return
+	// json 的才有效
+	res.send({
+		code: 300,
+		msg: "post_success",
+		data: req.body,
+	});
+});
+app.get("/api/signin", function (req, res) {
+	Sign();
+	res.send({
+		code: 200,
+		msg: "签到中",
+	});
+});
+app.get("/api/sse", function (req, res) {
+	// json 的才有效
+	res.setHeader("Content-Type", "text/event-stream");
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Cache-Control", "max-age=10");
+	res.setHeader("Connection", "keep-alive");
+
+	const interval = setInterval(() => {
+		res.write(`event:pin\n`);
+		res.write("data:{id:4Data chunk}\n\n");
+	}, 1000);
+
+	// 3秒后停止输出
+	setTimeout(() => {
+		if (res.writableEnded) {
+		} else {
+			clearInterval(interval);
+			res.write("data: Data end\n\n");
+		}
+	}, 3000);
 });
 
-app.get("/api/sse", function(req, res) {
-  // json 的才有效
-  res.setHeader('Content-Type', 'text/event-stream')
-  res.setHeader( 'Access-Control-Allow-Origin', '*')
-  res.setHeader("Cache-Control", "max-age=10")
-  res.setHeader("Connection", "keep-alive");
-
-  const interval = setInterval(() => {
-    res.write(`event:pin\n`)
-    res.write('data:{id:4Data chunk}\n\n');
-  }, 1000);
-
-  // 3秒后停止输出
-  setTimeout(() => {
-    if (res.writableEnded) {
-    } else {
-      clearInterval(interval);
-      res.write('data: Data end\n\n');
-    }
-  }, 3000);
-})
-
-
-
-const uploadsPath = path.resolve(__dirname, './public');
+const uploadsPath = path.resolve(__dirname, "./public");
 const storage = multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, uploadsPath);
-  },
-  filename: function (req, file, callback) {
-    const filename = req.headers['x-file-name'];
-    callback(null, `${Date.now()}.jpg`);
-    return;
-  }
-})
+	destination: function (req, file, callback) {
+		callback(null, uploadsPath);
+	},
+	filename: function (req, file, callback) {
+		const filename = req.headers["x-file-name"];
+		callback(null, `${Date.now()}.jpg`);
+		return;
+	},
+});
 const upload = multer({ storage });
-app.post("/api/file", upload.single('file'), (req, res) => {
-  console.log('req', req.files);
-  res.json({ url: `http://localhost:8088/public/${req.file}` })
-})
+app.post("/api/file", upload.single("file"), (req, res) => {
+	console.log("req", req.files);
+	res.json({ url: `http://localhost:8088/public/${req.file}` });
+});
 
-// get 发送短信 
+// get 发送短信
 // http://localhost:8088/api/email?subject=测试&to=electroluxcode@gmail.com&text=你好
-app.get('/api/email', function(req, res) {
-  if(req.query){
-    let data = JSON.parse(JSON.stringify(req.query))
-    console.log(data)
-    let {subject="默认标题",to="electroluxcode@gmail.com",text=`默认内容`} = data
-    SendMail(subject,to,text,res)
-  }
-  // res.send({
-  //   code:400,
-  //   msg:"发短信失败",
-  // });
+app.get("/api/email", function (req, res) {
+	if (req.query) {
+		let data = JSON.parse(JSON.stringify(req.query));
+		console.log(data);
+		let {
+			subject = "默认标题",
+			to = "electroluxcode@gmail.com",
+			text = `默认内容`,
+		} = data;
+		SendMail(subject, to, text, res);
+	}
+	// res.send({
+	//   code:400,
+	//   msg:"发短信失败",
+	// });
 });
 
-
-
-app.listen('8088', () => {
-  console.log('http:localhost:8088/api/get');
+app.listen("8088", () => {
+	console.log("http:localhost:8088/api/get");
 });
-
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
-
 
 module.exports = app;
--- behind/other/readme.md
+++ behind/other/readme.md
@@ -1,16 +1,3 @@
-mockReceiveServer
+自动签到脚本
 
-介绍
-
-mockReceiveServer。用express搭建的测试框架 两个接口：http://localhost:8088/post | http://localhost:8088/get
-
-
-数据埋点测试
-```
-data = ({
-  name: 'Berwin2222222222222'
-});
-
-window.navigator.sendBeacon('http://localhost:8088/post',data);
-
-```
+- cookie 失效全局搜索 tea_cookie 替换

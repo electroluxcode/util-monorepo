9528220b000ca218506c98e756d48980ba5198dd
Electrolux <3451613934@qq.com>
2024-04-07 20:19:56 +0800

    feat(view): 添加虚拟列表封装完全体

--- packages/ui/VisualList/VisualList.js
+++ packages/ui/VisualList/VisualList.js
@@ -1,3 +1,6 @@
+import { data } from "./data.js";
+// 这里可以设置插入的元素  p h1 h2 等都可以
+let createElement = "p";
 class DataProxy {
     Data;
     ProxyFn;
@@ -7,7 +10,8 @@ class DataProxy {
     }
     ProxyGet() {
         let that = this;
-        let ProxyRenderData = new Proxy(this.Data, {
+        this.Data.value = this.Data;
+        let ProxyRenderData = new Proxy(this.Data.value, {
             get: function (target, key) {
                 if (key in target) {
                     return target[key];
@@ -16,9 +20,17 @@ class DataProxy {
             set: function (target, key, value) {
                 // console.log("触发set", target, key, value)
                 target[key] = value;
-                if (that.ProxyFn) {
-                    Promise.OrderAll(that.ProxyFn, that.ProxyGet()).then((e) => {
-                        // console.log("promise代理的结果:", e)
+                if (key == "length") {
+                }
+                else {
+                    // console.log("触发set方法:", key, value, target);
+                    let container = document.querySelector(".inner");
+                    container.innerHTML = "";
+                    target.value.forEach((element) => {
+                        let p = document.createElement(createElement);
+                        p.innerHTML = element.id;
+                        container?.appendChild(p);
+                        console.log(element);
                     });
                 }
                 return true;
@@ -26,370 +38,57 @@ class DataProxy {
         });
         return ProxyRenderData;
     }
-    FnAdd(param) {
-        // 判断是不是promise
-        // if(typeof param.then==="function"){
-        //     this.ProxyFn.push(param)
-        //     return
-        // }
-        let temp = param.bind(this.ProxyGet());
-        this.ProxyFn.push(temp);
-    }
-}
-Promise.OrderAll = function (promiseArray, that) {
-    const arr = [];
-    return new Promise(async (resolve) => {
-        for (let index = 0; index < promiseArray.length; index++) {
-            console.log("that:", that);
-            let promiseItem = promiseArray[index].bind(that);
-            let res = await promiseItem();
-            arr.push(res);
-        }
-        resolve(arr);
-    });
-};
-/**
- * @des 检查 是不是 relative、absolute、fixed,不是就报错
- * @param this
- */
-function CheckLayout() {
-    // if(getComputedStyle(this.container).position!="relative"){
-    //     throw new Error("根组件布局有问题")
-    // }
 }
 /**
- * @des 需要维护两个数组 第一个数组是初始数组，第二个数组是渲染数组
+ * @des 数据源交给代理 需要维护两个数组 第一个数组是初始数组，第二个数组是渲染数组
+ * @des 基本dom结构需要三层 第一层是可视区(爷爷)，第二层基本不需要定义(父亲),第三层是我们的虚拟列表
  */
 class VisualList {
     ViewObject;
     // 代理数据
-    TrueRenderData;
     constructor(param) {
-        console.log(this);
-        // 初始化传参
-        this.ViewObject = {};
-        this.ViewObject.ParentElement = param.ParentElement;
-        this.ViewObject.ViewElement = param.ViewElement;
-        this.ViewObject.ChildrenElement = param.ChildrenElement;
-        this.TrueRenderData = this.ProxyGet({
-            init: {
-                status: "create",
-                data: param.data,
-            },
-        });
-        this.FirstRender(this.TrueRenderData);
-        // 初始化监听事件
-        this.ViewObject.ViewElement.addEventListener("scroll", () => {
-            // 尽量包括多一点东西
-            let BeforeElementCount = Math.floor(this.ViewObject.ViewElement.scrollTop /
-                this.ViewObject.SingleElementHeight);
-            console.log("????", BeforeElementCount, BeforeElementCount + this.ViewObject.ViewElementCount + 1);
-            this.TrueRenderData.init = {
-                data: param.data.slice(BeforeElementCount, BeforeElementCount + this.ViewObject.ViewElementCount + 1),
-                status: "update",
-                start: BeforeElementCount,
-            };
-            console.log({
-                data: param.data.slice(BeforeElementCount, BeforeElementCount + this.ViewObject.ViewElementCount + 1),
-                status: "update",
-                start: BeforeElementCount,
+        if (param.mode == "event") {
+            this.eventMode(param);
+        }
+    }
+    /**
+     * @des 需要使用者绑定一个render方法
+     * @des 注意使用者需要在render方法中进行可视区数据的绑定
+     * @param param
+     */
+    eventMode(param) {
+        // 初始渲染一个
+        param.render([param.data[0]]);
+        queueMicrotask(() => {
+            let ChildrenElement = document.querySelector(param.ChildrenElement);
+            let shouldDisplayCount = Math.floor(param.ViewElement.clientHeight / ChildrenElement.clientHeight);
+            shouldDisplayCount = shouldDisplayCount + 3;
+            // 定高设置
+            // ChildrenElement.parentElement.style.height =
+            // 	ChildrenElement.clientHeight * param.data.length + "px";
+            param.render(param.data.slice(0, 0 + shouldDisplayCount));
+            param.ViewElement.addEventListener("scroll", () => {
+                let ChildrenElement = document.querySelector(param.ChildrenElement);
+                let BeforeElementCount = Math.floor(param.ViewElement.scrollTop / ChildrenElement.clientHeight);
+                // 设置偏移量
+                ChildrenElement.parentElement.style.position = "relative";
+                ChildrenElement.parentElement.style.top =
+                    param.ViewElement.scrollTop + "px";
+                param.render(param.data.slice(BeforeElementCount, BeforeElementCount + shouldDisplayCount));
             });
         });
     }
-    ProxyGet(TrueRenderData) {
-        let ProxyRender = new DataProxy(TrueRenderData);
-        let ProxyRenderData = ProxyRender.ProxyGet();
-        let that = this;
-        ProxyRender.FnAdd(function () {
-            console.log("this:", this.init.data, this.init.start);
-            if (this.init.status == "update") {
-                that.ReRender(document.querySelector(".container_layer"), this.init.data, this.init.start, false);
-            }
-        });
-        return ProxyRenderData;
-    }
-    /**
-     * @des 用来双向绑定的
-     * @param ui
-     * @param data
-     * @param start
-     * @first 是不是第一次渲染
-     */
-    ReRender(ui, data, start, first) {
-        if (first) {
-            for (let i = 0; i < this.ViewObject.ViewElementCount; i++) {
-                // console.log(data["data"][i])
-                let text = document.createElement("p");
-                text.innerHTML = data[i].id;
-                text.style.top = `${i * this.ViewObject.SingleElementHeight}px`;
-                this.ViewObject.ParentElement.appendChild(text);
-            }
-            return;
-        }
-        let length = ui.children.length;
-        // console.log("data:",data["data"][0].id)
-        for (let i = 0; i < length; i++) {
-            // console.log(data["data"][i])
-            let temp = data[i].id;
-            ui.children[i].style.top = `${(i + start) * this.ViewObject.SingleElementHeight}px`;
-            ui.children[i].innerHTML = temp;
-            // debugger;
-        }
-    }
-    FirstRender(data) {
-        // 单个组件高度
-        this.ViewObject.ParentElement.innerHTML = this.ViewObject?.ChildrenElement;
-        this.ViewObject.SingleElementHeight =
-            this.ViewObject.ParentElement.children[0].getBoundingClientRect().height;
-        this.ViewObject.ParentElement.innerHTML = ``;
-        // 可视区初始化(需要计算的)
-        this.ViewObject.ViewElementHeight =
-            this.ViewObject.ViewElement.clientHeight;
-        this.ViewObject.BeforeElementCount = 0;
-        this.ViewObject.BeforeElementHeight = 0;
-        this.ViewObject.ViewElementCount = Math.ceil(this.ViewObject.ViewElementHeight / this.ViewObject.SingleElementHeight);
-        let AllRenderElementHeight = this.TrueRenderData?.init.data.length *
-            this.ViewObject.SingleElementHeight;
-        this.ViewObject.ParentElement.style.height = `${AllRenderElementHeight}px`;
-        // 渲染ui
-        let start = 0;
-        let ViewElementCount = this.ViewObject.ViewElementCount;
-        this.ReRender(document.querySelector(".container_layer"), this.TrueRenderData?.init.data.slice(0, ViewElementCount), 0, true);
-        let temp = [];
-        // children[0].clientHeight
-        for (let i = start; i <= ViewElementCount; i++) {
-            temp.push(data[i]);
-            console.log(this.ViewObject.ParentElement.children[i], "????");
-            // this.ViewObject.ParentElement.children[i].style.top = `${i * this.ViewObject.SingleElementHeight}px`
-            if (i == ViewElementCount) {
-                console.log("触发");
-                this.TrueRenderData.init = {
-                    data: temp,
-                    status: "create",
-                    start: 0,
-                };
-            }
-        }
-    }
 }
-// vi.call({
-//     container:document.querySelector(".container")!  as HTMLElement
-// });
-let data = [
-    { id: 0 },
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-    { id: 6 },
-    { id: 7 },
-    { id: 8 },
-    { id: 9 },
-    { id: 10 },
-    { id: 11 },
-    { id: 12 },
-    { id: 13 },
-    { id: 14 },
-    { id: 15 },
-    { id: 16 },
-    { id: 17 },
-    { id: 18 },
-    { id: 19 },
-    { id: 20 },
-    { id: 21 },
-    { id: 22 },
-    { id: 23 },
-    { id: 24 },
-    { id: 25 },
-    { id: 26 },
-    { id: 27 },
-    { id: 28 },
-    { id: 29 },
-    { id: 30 },
-    { id: 31 },
-    { id: 32 },
-    { id: 33 },
-    { id: 34 },
-    { id: 35 },
-    { id: 36 },
-    { id: 37 },
-    { id: 38 },
-    { id: 39 },
-    { id: 40 },
-    { id: 41 },
-    { id: 42 },
-    { id: 43 },
-    { id: 44 },
-    { id: 45 },
-    { id: 46 },
-    { id: 47 },
-    { id: 48 },
-    { id: 49 },
-    { id: 50 },
-    { id: 51 },
-    { id: 52 },
-    { id: 53 },
-    { id: 54 },
-    { id: 55 },
-    { id: 56 },
-    { id: 57 },
-    { id: 58 },
-    { id: 59 },
-    { id: 60 },
-    { id: 61 },
-    { id: 62 },
-    { id: 63 },
-    { id: 64 },
-    { id: 65 },
-    { id: 66 },
-    { id: 67 },
-    { id: 68 },
-    { id: 69 },
-    { id: 70 },
-    { id: 71 },
-    { id: 72 },
-    { id: 73 },
-    { id: 74 },
-    { id: 75 },
-    { id: 76 },
-    { id: 77 },
-    { id: 78 },
-    { id: 79 },
-    { id: 80 },
-    { id: 81 },
-    { id: 82 },
-    { id: 83 },
-    { id: 84 },
-    { id: 85 },
-    { id: 86 },
-    { id: 87 },
-    { id: 88 },
-    { id: 89 },
-    { id: 90 },
-    { id: 91 },
-    { id: 92 },
-    { id: 93 },
-    { id: 94 },
-    { id: 95 },
-    { id: 96 },
-    { id: 97 },
-    { id: 98 },
-    { id: 99 },
-    { id: 100 },
-    { id: 101 },
-    { id: 102 },
-    { id: 103 },
-    { id: 104 },
-    { id: 105 },
-    { id: 106 },
-    { id: 107 },
-    { id: 108 },
-    { id: 109 },
-    { id: 110 },
-    { id: 111 },
-    { id: 112 },
-    { id: 113 },
-    { id: 114 },
-    { id: 115 },
-    { id: 116 },
-    { id: 117 },
-    { id: 118 },
-    { id: 119 },
-    { id: 120 },
-    { id: 121 },
-    { id: 122 },
-    { id: 123 },
-    { id: 124 },
-    { id: 125 },
-    { id: 126 },
-    { id: 127 },
-    { id: 128 },
-    { id: 129 },
-    { id: 130 },
-    { id: 131 },
-    { id: 132 },
-    { id: 133 },
-    { id: 134 },
-    { id: 135 },
-    { id: 136 },
-    { id: 137 },
-    { id: 138 },
-    { id: 139 },
-    { id: 140 },
-    { id: 141 },
-    { id: 142 },
-    { id: 143 },
-    { id: 144 },
-    { id: 145 },
-    { id: 146 },
-    { id: 147 },
-    { id: 148 },
-    { id: 149 },
-    { id: 150 },
-    { id: 151 },
-    { id: 152 },
-    { id: 153 },
-    { id: 154 },
-    { id: 155 },
-    { id: 156 },
-    { id: 157 },
-    { id: 158 },
-    { id: 159 },
-    { id: 160 },
-    { id: 161 },
-    { id: 162 },
-    { id: 163 },
-    { id: 164 },
-    { id: 165 },
-    { id: 166 },
-    { id: 167 },
-    { id: 168 },
-    { id: 169 },
-    { id: 170 },
-    { id: 171 },
-    { id: 172 },
-    { id: 173 },
-    { id: 174 },
-    { id: 175 },
-    { id: 176 },
-    { id: 177 },
-    { id: 178 },
-    { id: 179 },
-    { id: 180 },
-    { id: 181 },
-    { id: 182 },
-    { id: 183 },
-    { id: 184 },
-    { id: 185 },
-    { id: 186 },
-    { id: 187 },
-    { id: 188 },
-    { id: 189 },
-    { id: 190 },
-    { id: 191 },
-    { id: 192 },
-    { id: 193 },
-    { id: 194 },
-    { id: 195 },
-    { id: 196 },
-    { id: 197 },
-    { id: 198 },
-    { id: 199 },
-    { id: 195 },
-    { id: 196 },
-    { id: 197 },
-    { id: 198 },
-    { id: 199 },
-    { id: 195 },
-    { id: 196 },
-    { id: 197 },
-    { id: 198 },
-    { id: 199 },
-];
+let test = new DataProxy([]);
+let case1 = test.ProxyGet();
 new VisualList({
-    data,
     ParentElement: document.querySelector(".container_layer"),
-    ChildrenElement: "<p>22</p>",
+    ChildrenElement: createElement,
     ViewElement: document.querySelector(".container"),
+    data,
+    mode: "event",
+    render: (data) => {
+        console.log("render:", data);
+        case1.value = data;
+    },
 });
